80330e79fb21e5554c639a822c50245d1e827425
tanguopeng <guopengtan@rastar.com>
2018-09-14 19:17:23 +0800

    多页面打包

---
+++ src/views/aaa/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="asd"> 
+ sdffffffffffffffffff
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        
+      }
+    }
+}
+</script>
+<style lang="scss" src="./style.scss" scoped></style>
---
+++ src/views/bgb/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="asd"> 
+ 
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+        
+      }
+    }
+}
+</script>
+<style lang="scss" src="./style.scss" scoped></style>
